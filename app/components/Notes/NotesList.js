3cc0bd1ba0f717a5fcc5129a01b1edbd03ce454e
EarvinKayonga <demoneartin@gmail.com>
2016-03-14 13:46:44 +0100

    mini functional

---
+++ app/components/Notes/NotesList.js
@@ -0,0 +1,19 @@
+var React       = require('react');
+
+var NotesList   = React.createClass({
+    render: function(){
+        var notes = this.props.notes.map(function(notes, index){
+            return <li className="list-group-item" key={index}>{notes['.value']}</li>
+        });
+        
+        return (
+            <ul className="list-group">
+                {notes}
+            </ul>
+        );
+    }
+});
+
+module.exports = NotesList;
+
+// .value => fireBase
---
+++ app/components/Profile.js
@@ -0,0 +1,62 @@
+var React       = require('react'),
+    Router      = require('react-router'),
+    
+    
+    Repos       = require('./Github/Repos'),
+    UserProfile = require('./Github/UserProfile'),
+    Notes       = require('./Notes/Notes'),
+    
+    ReactFireMixin = require('reactfire'),
+    Firebase    = require('firebase');
+    
+    
+var Profile     = React.createClass({
+    mixins: [ReactFireMixin],
+    
+    getInitialState: function(){
+        return {
+            notes: [1, 2, 3],
+            bio:   {
+                'name': 'Earvin Kayonga',
+            },
+            repos: ['a', 'b', 'c']
+        }
+    },
+    
+    componentDidMount: function(){
+        this.ref = new Firebase('https://earvin-git.firebaseio.com');
+        var childRef = this.ref.child(this.props.params.username);
+        this.bindAsArray(childRef, 'notes');
+    },
+    
+    componentWillUnMount: function(){
+        this.unbind('notes');
+    },
+    
+    render: function(){
+        return (
+            <div className="row">
+                <div className="col-md-4">
+                    <UserProfile 
+                        username={this.props.params.username}
+                        bio={this.state.bio}
+                    />
+                </div>
+                <div className="col-md-4">
+                    <Repos
+                        username={this.props.params.username}
+                        repos={this.state.repos}
+                    />
+                </div>
+                <div className="col-md-4">
+                    <Notes 
+                        username={this.props.params.username}
+                        notes={this.state.notes}
+                    />
+                </div>
+            </div>
+        )
+    }
+});
+
+module.exports  = Profile;
